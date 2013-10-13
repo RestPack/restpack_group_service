94bc73c182b4caaa22afce4a78082fb78364cf22
Gavin Joyce <gavinjoyce@gmail.com>
2013-10-13 13:30:27 +0100

    spec_helper doesnt need to be relative

--- spec/commands/group/create_spec.rb
+++ spec/commands/group/create_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../../spec_helper'
+require 'spec_helper'
 
 describe Commands::Group::Create do
   it_acts_as_create_command(:group)
--- spec/commands/membership/list_spec.rb
+++ spec/commands/membership/list_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../../spec_helper'
+require 'spec_helper'
 
 describe Commands::Membership::List do
   is_required :application_id
